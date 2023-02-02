a44279be9395fd30eb6bc6f15a3cd2f273bccd44
mauro079476 <5090@holbertonstudents.com>
2023-02-02 06:04:21 -0800

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"" creating an empty class""
+""" creating an empty class"""
 
-class Square
-    "" this is an empty class""
+class Square:
+    """ this is an empty class"""
     pass
