34fc0af871c7f3291196c67e1ed48abd483ea205
mauro079476 <5090@holbertonstudents.com>
2023-02-17 14:24:35 -0800

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """python interpreter"""
+
+Class Base:
+    """class"""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        if id si not None:
+            self.id = id
