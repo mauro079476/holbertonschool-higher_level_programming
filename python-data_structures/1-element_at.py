885efd490f829ead3fbaaf691c6eda27e12ae092
mauro079476 <5090@holbertonstudents.com>
2023-03-22 10:51:45 -0700

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    idx_max = len(my_list) - 1
-    if ((idx < 0) or (idx > idx_max)):
-                return Non
-            return my_list[idx]
+        if idx < 0:
+            return (None)
+
+        if len(my_list) <= idx:
+            return (None)
+        else:
+            return (my_list[idx])
