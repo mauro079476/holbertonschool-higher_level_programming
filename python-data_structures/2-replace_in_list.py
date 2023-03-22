ee8fd2dda05f628908d14457e550186f171aa164
mauro079476 <5090@holbertonstudents.com>
2023-03-22 11:13:38 -0700

    fixed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    size = len (my_list) -1
+    size = len(my_list) - 1
     if idx < 0 or idx > size:
         return (my_list)
     else:
