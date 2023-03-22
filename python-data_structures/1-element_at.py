f2cbf0eaf0e98ba026d30c9f67506cfbf1121f0b
mauro079476 <5090@holbertonstudents.com>
2023-03-22 10:48:07 -0700

    fix

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    idx_max = len(my_list) - 1
+    if ((idx < 0) or (idx > idx_max)):
+                return Non
+            return my_list[idx]
